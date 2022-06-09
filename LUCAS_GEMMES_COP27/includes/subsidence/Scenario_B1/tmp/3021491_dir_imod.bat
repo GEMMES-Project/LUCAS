e4e016bea8f656738b27be8243613d7987d8dd6c
Quang C. Truong <tcquang@ctu.edu.vn>
2022-06-09 23:56:13 +0700

    Update  subsidence data - nodata

--- LUCAS_GEMMES_COP27/includes/subsidence/Scenario_B1/tmp/3021491_dir_imod.bat
+++
@@ -1,1 +0,0 @@
-dir /b /o "D:\TEMP\SIMULATED_CUMULATIVE_SUBSIDENCE_MKD_SINCE_2018\SCENARIO_B1\*.IDF" > "d:\Temp\Simulated_cumulative_subsidence_MKD_since_2018\Scenario_B1\tmp\3021491_dir_imod.txt"
--- LUCAS_GEMMES_COP27/includes/subsidence/Scenario_B2/tmp/3021491_dir_imod.bat
+++
@@ -1,1 +0,0 @@
-dir /b /o "D:\TEMP\SIMULATED_CUMULATIVE_SUBSIDENCE_MKD_SINCE_2018\SCENARIO_B2\*.IDF" > "d:\Temp\Simulated_cumulative_subsidence_MKD_since_2018\Scenario_B2\tmp\3021491_dir_imod.txt"
